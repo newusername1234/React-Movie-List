6b82476aa2097a8c151578a21f784c223895ed4e
Wes <wesleyayer92@gmail.com>
2020-01-31 16:03:01 -0500

    adds set state functionality to movies class

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,13 +1,58 @@
 import React from 'react';
 
-function MovieList() {
-    return (
-        <ul>
-            <li>Star Wars</li>
-            <li>Indiana Jones</li>
-            <li>Blade Runner</li>
-        </ul>
-    )
+
+
+function _handleMouseOver(event) {
+    console.log('Moused over');
+    console.log(event.target);
 }
+
+// const movies = [
+//     'The Goonies',
+//     'E.T.',
+//     'Gleaming the Cube'
+// ]
+
+// "MovieList is a React Component"
+class MovieList extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: [
+                'The Goonies',
+                'E.T.',
+                'Gleaming the Cube'
+            ]
+        };
+    }
+
+    render() {
+        return (
+            <ul>
+                {
+                    this.state.movies.map((movie, i) => (
+                    <li onClick={() => {
+                        this._handleClick(i)
+                    }}>{movie}</li>
+                    ))
+                }
+            </ul>
+        )
+    }
+
+    _handleClick = (index) => {
+        console.log(index);
+        const newMovies = [
+            ...this.state.movies
+        ];
+        newMovies[index] += '!';
+
+        this.setState({
+            movies: newMovies
+        });
+    }
+}
+
 
 export default MovieList;
